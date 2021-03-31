f563e37fb3da0dc7b1ba4799d0d200b30395156c
cuhte3 <sintezzlab@gmail.com>
2021-03-31 15:46:39 +0300

    Services

---
+++ Shared/Services/MarketDataRepository.swift
@@ -0,0 +1,120 @@
+//
+//  MarketDataRepository.swift
+//  Portal
+//
+//  Created by Farid on 14.04.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+final class MarketDataRepository  {    
+    typealias CoinCode = String
+    typealias CurrencyCode = String
+    typealias Rate = Double
+    
+    private var marketDataUpdater = MarketDataUpdater()
+    private var ratesUpdater = RatesDataUpdater(interval: 60)
+    private var priceUpdater = PricesDataUpdater(interval: 60)
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    private var currencyRates = Synchronized([CurrencyCode : Rate]())
+    private var marketData = Synchronized([CoinCode : CoinMarketData]())
+        
+    init() {
+        bindServices()
+    }
+    
+    private func bindServices() {
+        marketDataUpdater.onUpdatePublisher
+            .sink(receiveValue: { [weak self] (range, data) in
+                guard let self = self else { return }
+                self.update(range, data)
+            })
+            .store(in: &cancellables)
+        
+        ratesUpdater.onUpdatePublisher
+            .sink(receiveValue: { [weak self] rates in
+                guard let self = self else { return }
+                self.update(rates)
+            })
+            .store(in: &cancellables)
+        
+        priceUpdater.onUpdatePublisher
+            .sink(receiveValue: { [weak self] prices in
+                guard let self = self else { return }
+                self.update(prices)
+            })
+            .store(in: &cancellables)
+    }
+    
+    func data(for coin: String) -> CoinMarketData {
+        marketData.value[coin] ?? CoinMarketData()
+    }
+
+    func rate(for currency: FiatCurrency) -> Double {
+        let code = currency.code
+        return currencyRates.value[code] ?? 1.0
+    }
+    
+    func pause() {
+        ratesUpdater.pause()
+        priceUpdater.pause()
+    }
+    
+    func resume() {
+        ratesUpdater.resume()
+        priceUpdater.resume()
+    }
+}
+
+extension MarketDataRepository {
+    private func update(_ range: MarketDataRange, _ data: HistoricalDataResponse) {
+        for points in data {
+            if self.marketData.value[points.key] == nil {
+                self.marketData.writer({ (data) in
+                    data[points.key] = CoinMarketData()
+                })
+            }
+            self.marketData.writer({ (data) in
+                switch range {
+                case .hour:
+                    data[points.key]?.hourPoints = points.value
+                case .day:
+                    data[points.key]?.dayPoints = points.value
+                case .week:
+                    data[points.key]?.weekPoints = points.value
+                case .month:
+                    data[points.key]?.monthPoints = points.value
+                case .year:
+                    data[points.key]?.yearPoints = points.value
+                }
+            })
+        }
+    }
+    
+    private func update(_ rates: Rates) {
+        currencyRates.writer({ (data) in
+            data = rates.filter{$0.key != "BTC"}
+        })
+    }
+    
+    private func update(_ prices: PriceResponse) {
+        for price in prices {
+            if marketData.value[price.key] == nil {
+                marketData.writer({ (data) in
+                    data[price.key] = CoinMarketData()
+                })
+            }
+            for currency in price.value {
+                marketData.writer({ (data) in
+                    data[price.key]?.priceData = currency.value
+                })
+            }
+        }
+    }
+}
+
---
+++ Shared/Services/PricesDataUpdater.swift
@@ -0,0 +1,76 @@
+//
+//  PricesDataUpdater.swift
+//  Portal
+//
+//  Created by Farid on 16.04.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+typealias PriceResponse = Dictionary<String, Dictionary<String, MarketPrice>>
+
+final class PricesDataUpdater: IPricesData {
+    var onUpdatePublisher = PassthroughSubject<PriceResponse, Never>()
+    
+    private let jsonDecoder: JSONDecoder
+    private let timer: RepeatingTimer
+    private var task: URLSessionTask?
+        
+    init(
+        jsonDecoder: JSONDecoder = JSONDecoder(),
+        interval: TimeInterval
+    ) {
+        self.jsonDecoder = jsonDecoder
+
+        self.timer = RepeatingTimer(timeInterval: interval)
+        self.timer.eventHandler = { [weak self] in
+            guard self?.task?.state != .running else { return }
+            
+            self?.updatePrices(for: "") { result in
+                guard let self = self else { return }
+                switch result {
+                case let .success(prices):
+                    self.onUpdatePublisher.send(prices)
+                case let .failure(error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        self.timer.resume()
+    }
+            
+    func updatePrices(for assets: String, _ competionHandler: @escaping ((Result<PriceResponse, NetworkError>) -> Void)) {
+        guard let url = URL(string: "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR") else {
+            competionHandler(.failure(.networkError))
+            return
+        }
+        task = URLSession.shared.dataTask(with: url) { data, response, error in
+            switch (data, error) {
+            case (_, .some):
+                competionHandler(.failure(.networkError))
+            case let (.some(data), nil):
+                guard let mockResponse = fullPriceResponse.data(using: .utf8) else {
+                    return
+                }
+                guard let response = try? self.jsonDecoder.decode(PriceResponse.self, from: mockResponse) else {
+                    competionHandler(.failure(.parsing))
+                    return
+                }
+                competionHandler(.success(response))
+            case (nil, nil):
+                competionHandler(.failure(.inconsistentBehavior))
+            }
+        }
+        task?.resume()
+    }
+    
+    func pause() {
+        timer.suspend()
+    }
+    
+    func resume() {
+        timer.resume()
+    }
+}
