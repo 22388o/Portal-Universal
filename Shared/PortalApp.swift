270b76714897ed95e1d0c39f8bfca71dcb6f3b1b
cuhte3 <sintezzlab@gmail.com>
2021-06-07 14:22:47 +0300

    passing wallet service, marketData per down the view hierarchy as environmentObjects

--- Shared/PortalApp.swift
+++ Shared/PortalApp.swift
@@ -10,14 +10,18 @@ import SwiftUI
 @main
 struct PortalApp: App {
     private let walletsService: WalletsService
+    private let marketDataRepository: MarketDataRepository
     
     init() {
         walletsService = WalletsService(context: PersistenceController.shared.container.viewContext)
+        marketDataRepository = MarketDataRepository()
     }
 
     var body: some Scene {
         WindowGroup {
-            RootView(walletService: walletsService)
+            RootView()
+                .environmentObject(walletsService)
+                .environmentObject(marketDataRepository)
                 .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
                 .edgesIgnoringSafeArea(.all)
         }
