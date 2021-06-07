f1142b12128ff9a226c061e0863629831fa593ac
cuhte3 <sintezzlab@gmail.com>
2021-06-07 14:17:33 +0300

    remove unnecessarily code

---
+++ Shared/Services/Providers/MarketDataProvider.swift
@@ -0,0 +1,27 @@
+//
+//  MarketDataProvider.swift
+//  Portal
+//
+//  Created by Farid on 15.05.2021.
+//
+
+import Foundation
+import Coinpaprika
+
+//final class MarketDataProvider: IMarketDataProvider {
+//    var ticker: Ticker?
+//    
+//    init(coin: Coin) {
+//        Coinpaprika.API.ticker(id: "btc-bitcoin", quotes: [.usd, .btc, .eth]).perform { (response) in
+//            switch response {
+//              case .success(let ticker):
+//                print(ticker[.usd].price)
+//                self.ticker = ticker
+//              case .failure(let error):
+//                print(error)
+//            }
+//        }
+//    }
+//}
+
+
