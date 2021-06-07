99284bf0a9a59ab44e9d3042bccf545db218aaa0
cuhte3 <sintezzlab@gmail.com>
2021-06-07 14:20:42 +0300

    CoinMarketData + MarketPrice + MarketSnapshotdouble replaced with decimal

--- Shared/Models/MarketSnapshot.swift
+++ Shared/Models/MarketSnapshot.swift
@@ -7,34 +7,42 @@
 //
 
 import Foundation
+import Coinpaprika
 
 struct MarketSnapshot {
-    let high: Double
-    let low: Double
-    let open: Double
-    let close: Double
+    let high: Decimal
+    let low: Decimal
+    let open: Decimal
+    let close: Decimal
+    
+    init(_ ohlc: Coinpaprika.Ohlcv) {
+        high = ohlc.high ?? 0
+        low = ohlc.low ?? 0
+        open = ohlc.open ?? 0
+        close = ohlc.close ?? 0
+    }
 }
 
-extension MarketSnapshot: Decodable {
-    enum Keys: String, CodingKey {
-        case high
-        case low
-        case close
-        case open
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: Keys.self)
-        
-        let h = try container.decode(Double.self, forKey: .high)
-        let l = try container.decode(Double.self, forKey: .low)
-        let o = try container.decode(Double.self, forKey: .open)
-        let c = try container.decode(Double.self, forKey: .close)
-        
-        high = h
-        low = l
-        open = o
-        close = c
-    }
-}
+//extension MarketSnapshot: Decodable {
+//    enum Keys: String, CodingKey {
+//        case high
+//        case low
+//        case close
+//        case open
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: Keys.self)
+//
+//        let h = try container.decode(Double.self, forKey: .high)
+//        let l = try container.decode(Double.self, forKey: .low)
+//        let o = try container.decode(Double.self, forKey: .open)
+//        let c = try container.decode(Double.self, forKey: .close)
+//
+//        high = h
+//        low = l
+//        open = o
+//        close = c
+//    }
+//}
 
