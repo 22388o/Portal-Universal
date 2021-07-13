ffcca93b4bd01bdaf8664d6d2089d79fd4974f44
cuhte3 <sintezzlab@gmail.com>
2021-07-13 13:45:48 +0300

    Asset adapters support implementation BTC related code moved to Bitcoin adapter

--- Shared/Core/Asset.swift
+++ Shared/Core/Asset.swift
@@ -8,229 +8,136 @@
 
 import Foundation
 import Combine
-
-final class TxProvider: ITxProvider {
-    let coin: Coin
-    let kit: ICoinKit
-    
-    init(coin: Coin, kit: ICoinKit) {
-        self.coin = coin
-        self.kit = kit
-    }
-}
-protocol ITxProvider {}
-
-import BitcoinKit
+import BitcoinCore
 import RxSwift
 import Hodler
 
 final class Asset: IAsset {
-    var id: UUID
-    var coin: Coin
-    var kit: AbstractKit?
-    var marketDataProvider: IMarketDataProvider
-    var balanceProvider: IBalanceProvider
-    var chartDataProvider: IChartDataProvider
-    var marketChangeProvider: IMarketChangeProvider
-    var qrCodeProvider: IQRCodeProvider
-    var balanceStatePublisher: Published<KitSyncState>.Publisher { $balanceState }
-    var balanceUpdatedSubject = PassthroughSubject<BalanceInfo, Never>()
-    var txStatePublisher: Published<KitSyncState>.Publisher { $transactionState }
+    private(set) var id: UUID
+    private(set) var coin: Coin
     
-    @Published private var balanceState: KitSyncState {
-        didSet {
-            transactionState = balanceState
-        }
-    }
-    @Published private var transactionState: KitSyncState
-//    @Published private var balanceUpdated: Void
+    private(set) var adapter: IAdapter?
+    private(set) var balanceAdapter: IBalanceAdapter?
+    private(set) var depositAdapter: IDepositAdapter?
+    private(set) var transactionAdaper: ITransactionsAdapter?
+    private(set) var sendBtcAdapter: ISendBitcoinAdapter?
     
-    let coinRate: Decimal = pow(10, 8)
-    
-    init(coin: Coin, walletID: UUID, seed: [String] = [], btcAddressFormat: BtcAddressFormat, kit: AbstractKit? = nil) {
+    private(set) var marketDataProvider: IMarketDataProvider
+    private(set) var qrCodeProvider: IQRCodeProvider
+        
+    init(coin: Coin, walletID: UUID, seed: [String] = [], btcAddressDereviation: MnemonicDerivation) {
         self.id = UUID()
         self.coin = coin
-        
-        self.balanceState = .notSynced(error: PError.unknow)
-        self.transactionState = .notSynced(error: PError.unknow)
-        
-        let walletID = "\(walletID)_\(coin.name.lowercased())_wallet_id"
         
         if !seed.isEmpty {
             switch coin.type {
             case .bitcoin:
-                let bip: Bip
+                let walletID = "\(walletID)_\(coin.name.lowercased())_wallet_id"
                 
-                switch btcAddressFormat {
-                case .legacy:
-                    bip = .bip44
-                case .segwit:
-                    bip = .bip49
-                case .nativeSegwit:
-                    bip = .bip49//.bip84
-                }
+                let kit = try? BitcoinAdapter(walletID: walletID, seed: seed, dereviation: btcAddressDereviation, syncMode: .fast, testMode: true)
                 
-                self.kit = try? BitcoinKit(
-                    withWords: seed,
-                    bip: bip,
-                    walletId: walletID,
-                    syncMode: .api,
-                    networkType: .testNet,
-                    confirmationsThreshold: 0,
-                    logger: .init(minLogLevel: .error)
-                )
+                self.adapter = kit
+                self.balanceAdapter = kit
+                self.depositAdapter = kit
+                self.transactionAdaper = kit
+                self.sendBtcAdapter = kit
+                 
             case .etherium:
                 break
             default:
                 break
             }
-        } else {
-            self.kit = nil
         }
                 
         self.marketDataProvider = MarketDataProvider(coin: coin)
-        self.balanceProvider = BalanceProvider(coin: coin, kit: kit)
-        self.chartDataProvider = ChartDataProvider()
-        self.marketChangeProvider = MarketChangeProvider()
         self.qrCodeProvider = QRCodeProvider()
         
         start()
     }
     
     private func start() {
-        switch coin.type {
-        case .bitcoin:
-            (self.kit as? BitcoinKit)?.delegate = self
-            self.kit?.start()
-        default:
-            break
-        }
-    }
-    
-    static func bitcoin() -> IAsset {
-        Asset(coin: Coin.bitcoin(), walletID: UUID(), btcAddressFormat: .segwit)
+        self.adapter?.start()
     }
     
     func availableBalance(feeRate: Int, address: String?) -> Decimal {
-        let amount = (try? kit?.maxSpendableValue(toAddress: address, feeRate: feeRate, pluginData: [:])) ?? 0
-        return Decimal(amount) / coinRate
+        switch coin.type {
+        case .bitcoin:
+            return sendBtcAdapter?.availableBalance(feeRate: feeRate, address: address, pluginData: [:]) ?? 0
+        case .etherium:
+            return 0
+        case .erc20( _):
+            return 0
+        }
     }
 
     func maximumSendAmount() -> Decimal? {
-        try? kit?.maxSpendLimit(pluginData: [:]).flatMap { Decimal($0) / coinRate }
+        switch coin.type {
+        case .bitcoin:
+            return sendBtcAdapter?.maximumSendAmount(pluginData: [:])
+        case .etherium:
+            return nil
+        case .erc20( _):
+            return nil
+        }
     }
 
     func minimumSendAmount(address: String?) -> Decimal {
-        Decimal(kit?.minSpendableValue(toAddress: address) ?? 0) / coinRate
+        switch coin.type {
+        case .bitcoin:
+            return sendBtcAdapter?.minimumSendAmount(address: address) ?? 0
+        case .etherium:
+            return 0
+        case .erc20( _):
+            return 0
+        }
     }
 
     func validate(address: String) throws {
-        try kit?.validate(address: address, pluginData: [:])
+        switch coin.type {
+        case .bitcoin:
+            try sendBtcAdapter?.validate(address: address, pluginData: [:])
+        case .etherium:
+            break
+        case .erc20( _):
+            break
+        }
     }
 
     func fee(amount: Decimal, feeRate: Int, address: String?) -> Decimal {
-        do {
-            let amount = convertToSatoshi(value: amount)
-            let fee = try kit?.fee(for: amount, toAddress: address, feeRate: feeRate, pluginData: [:]) ?? 0
-            return Decimal(fee) / coinRate
-        } catch {
+        switch coin.type {
+        case .bitcoin:
+            return sendBtcAdapter?.fee(amount: amount, feeRate: feeRate, address: address, pluginData: [:]) ?? 0
+        case .etherium:
+            return 0
+        case .erc20( _):
             return 0
         }
     }
 
     func send(amount: Decimal, address: String, feeRate: Int, sortMode: TransactionDataSortMode) -> Future<Void, Error> {
-        let satoshiAmount = convertToSatoshi(value: amount)
-        let sortType = convertToKitSortMode(sort: sortMode)
-
         return Future { [weak self] promisse in
-            do {
-                print("Sending to \(address)")
-                _ = try self?.kit?.send(to: address, value: satoshiAmount, feeRate: feeRate, sortType: sortType, pluginData: [:])
+            print("Sending to \(address)")
+            
+            switch self?.coin.type {
+            case .bitcoin:
+                _ = self?.sendBtcAdapter?.sendSingle(amount: amount, address: address, feeRate: feeRate, pluginData: [:], sortMode: sortMode)
                 promisse(.success(()))
-            } catch {
-                promisse(.failure(error))
+            case .etherium:
+                promisse(.failure(AppError.unknownError))
+            case .erc20( _):
+                promisse(.failure(AppError.unknownError))
+            case .none:
+                promisse(.failure(AppError.unknownError))
             }
-        }
-    }
-
-    var statusInfo: [(String, Any)] {
-        kit?.statusInfo ?? []
-    }
-    
-    private func convertToSatoshi(value: Decimal) -> Int {
-        let coinValue: Decimal = value * coinRate
-        let handler = NSDecimalNumberHandler(roundingMode: .plain, scale: Int16(truncatingIfNeeded: 0), raiseOnExactness: false, raiseOnOverflow: false, raiseOnUnderflow: false, raiseOnDivideByZero: false)
-        return NSDecimalNumber(decimal: coinValue).rounding(accordingToBehavior: handler).intValue
-    }
-    
-    private func convertToKitSortMode(sort: TransactionDataSortMode) -> TransactionDataSortType {
-        switch sort {
-        case .shuffle: return .shuffle
-        case .bip69: return .bip69
         }
     }
 }
 
-extension Asset: BitcoinCoreDelegate {
-    func transactionsUpdated(inserted: [TransactionInfo], updated: [TransactionInfo]) {
-        print("inserted: \(inserted)")
-        let insertedTxs = inserted.map{ PortalTx(transaction: $0, lastBlockInfo: kit?.lastBlockInfo)}
-        for tx in insertedTxs {
-            NotificationService.shared.add(PNotification(message: "New transaction: \(tx.amount) BTC"))
-        }
-    }
-    
-    func transactionsDeleted(hashes: [String]) {
-    }
-    
-    func balanceUpdated(balance: BalanceInfo) {
-        balanceUpdatedSubject.send(balance)
-    }
-    
-    func lastBlockInfoUpdated(lastBlockInfo: BlockInfo) {
-        print("last block: \(lastBlockInfo)")
-    }
-    
-    func kitStateUpdated(state: BitcoinCore.KitState) {
-        switch state {
-        case .synced:
-            if case .synced = self.balanceState {
-                return
-            }
-    
-            print("Synced!")
-
-            self.balanceState = .synced
-        case .notSynced(let error):
-            print(error.localizedDescription)
-            if case .notSynced(let appError) = self.balanceState, "\(error)" == "\(appError)" {
-                return
-            }
-
-            self.balanceState = .notSynced(error: error)
-        case .syncing(let progress):
-            let newProgress = Int(progress * 100)
-            let newDate = kit?.lastBlockInfo?.timestamp.map { Date(timeIntervalSince1970: Double($0)) }
-
-            if case let .syncing(currentProgress, currentDate) = self.balanceState, newProgress == currentProgress {
-                if let currentDate = currentDate, let newDate = newDate, currentDate.isSameDay(as: newDate) {
-                    return
-                }
-            }
-
-            self.balanceState = .syncing(progress: newProgress, lastBlockDate: newDate)
-        case .apiSyncing(let newCount):
-            if case .searchingTxs(let count) = self.balanceState, newCount == count {
-                return
-            }
-
-            self.balanceState = .searchingTxs(count: newCount)
-        }
+extension Asset {
+    static func bitcoin() -> IAsset {
+        Asset(coin: Coin.bitcoin(), walletID: UUID(), btcAddressDereviation: .bip49)
     }
 }
-
-import BitcoinCore
-import Hodler
 
 protocol IBitcoinPluginData: IPluginData {}
 
