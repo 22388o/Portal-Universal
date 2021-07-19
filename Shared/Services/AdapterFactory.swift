92b42d830ccdb6f802e75b08d46bbaacc648526b
cuhte3 <sintezzlab@gmail.com>
2021-07-19 16:15:30 +0300

    Create AdapterFactory.swift

---
+++ Shared/Services/AdapterFactory.swift
@@ -0,0 +1,36 @@
+//
+//  AdapterFactory.swift
+//  Portal
+//
+//  Created by Farid on 19.07.2021.
+//
+
+import Foundation
+
+class AdapterFactory: IAdapterFactory {
+    private let appConfigProvider: IAppConfigProvider
+    private let ethereumKitManager: EthereumKitManager
+
+    init(appConfigProvider: IAppConfigProvider, ethereumKitManager: EthereumKitManager) {
+        self.appConfigProvider = appConfigProvider
+        self.ethereumKitManager = ethereumKitManager
+    }
+
+    func adapter(wallet: Wallet) -> IAdapter? {
+        switch wallet.coin.type {
+        case .bitcoin:
+            return try? BitcoinAdapter(wallet: wallet, syncMode: .fast, testMode: appConfigProvider.testMode)
+        case .ethereum:
+            if let evmKit = try? ethereumKitManager.evmKit(account: wallet.account) {
+                return EvmAdapter(evmKit: evmKit)
+            }
+        case let .erc20(address):
+            if let evmKit = try? ethereumKitManager.evmKit(account: wallet.account) {
+                return try? Evm20Adapter(evmKit: evmKit, contractAddress: address, decimal: wallet.coin.decimal)
+            }
+        }
+
+        return nil
+    }
+
+}
