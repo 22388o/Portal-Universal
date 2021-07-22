e4580556d6059ebeb1722544e798880c16f67d5a
cuhte3 <sintezzlab@gmail.com>
2021-07-22 17:59:34 +0300

    Update RootView.swift

--- Shared/Views/RootView.swift
+++ Shared/Views/RootView.swift
@@ -8,54 +8,19 @@
 import SwiftUI
 
 struct RootView: View {
-    @State private var hideLoader = false
-    @State private var loaderIsPresented = true
-    
-    @EnvironmentObject private var marketDataProvider: MarketDataProvider
     @ObservedObject private var state = Portal.shared.state
         
     var body: some View {
         ZStack {
             Color.portalWalletBackground
             
-            Group {
-                switch state.current {
-                case .currentWallet:
-                    WalletScene()
-
-                    if loaderIsPresented {
-                        PortalLoader()
-                            .scaleEffect(hideLoader ? 10 : 1)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .background(Color.portalWalletBackground)
-                            .opacity(hideLoader ? 0 : 1)
-                    }
-                case .createWallet:
-                    CreateWalletScene()
-                case .restoreWallet:
-                    RestoreWalletView()
-                }
-            }
-            .transition(AnyTransition.scale.combined(with: .opacity))
-        }
-        .onReceive(Portal.shared.$marketDataReady.dropFirst(), perform: { dataIsLoaded in
-            DispatchQueue.main.asyncAfter(deadline: .now()) {
-                withAnimation {
-                    hideLoader.toggle()
-                }
-            }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                loaderIsPresented.toggle()
-                hideLoader.toggle()
-            }
-        })
-        .onReceive(Portal.shared.accountManager.onActiveAccountUpdatePublisher) { account in
-            if account != nil {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                    withAnimation {
-                        state.current = .currentWallet
-                    }
-                }
+            switch state.current {
+            case .currentAccount:
+                WalletScene()
+            case .createAccount:
+                CreateWalletScene()
+            case .restoreAccount:
+                RestoreWalletView()
             }
         }
     }
