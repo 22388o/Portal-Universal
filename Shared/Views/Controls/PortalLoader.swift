ead33adc025b1053d40c9253043dea0a052f0209
cuhte3 <sintezzlab@gmail.com>
2021-07-22 17:59:11 +0300

    PortalLoader animation improvements

--- Shared/Views/Controls/PortalLoader.swift
+++ Shared/Views/Controls/PortalLoader.swift
@@ -25,30 +25,62 @@ private class Counter: ObservableObject {
     }
 }
 
-struct PortalLoader: View {
+extension View {
+    func isLocked(locked: Binding<Bool>) -> ModifiedContent<Self, LoadingViewModifier> {
+        modifier(LoadingViewModifier(locked: locked))
+    }
+}
+
+struct LoadingViewModifier: ViewModifier {
+    @Binding var locked: Bool
+            
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            
+            if locked {
+                LoadingAnimationView()
+                    .zIndex(1)
+                    .transition(
+                        .asymmetric(insertion: .slide, removal: .scale(scale: 15)
+                        .combined(with: AnyTransition.opacity))
+                        .animation(.easeInOut(duration: 0.5))
+                    )
+            }
+        }
+    }
+}
+
+struct LoadingAnimationView: View {
     private let frames = (0...59).compactMap { Image("loading-frame_\($0)") }
     
     @ObservedObject private var counter = Counter(interval: 0.05)
     @State private var animating: Bool = false
+    @State private var onHide: Bool = false
     
     var body: some View {
-        frames[counter.value % frames.count]
-            .resizable()
-            .frame(width: 250, height: 250, alignment: .center)
-            .scaleEffect(animating ? 1 : 0.7)
-            .onAppear {
-                withAnimation(Animation.easeInOut(duration: 0.4).repeatForever(autoreverses: true), {
-                    animating.toggle()
-                })
-            }
-            .onDisappear {
-                counter.reset()
-            }
+        ZStack {
+            Color.portalWalletBackground
+            
+            frames[counter.value % frames.count]
+                .resizable()
+                .frame(width: 250, height: 250, alignment: .center)
+                .scaleEffect(animating ? 1 : 0.7)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 0.4).repeatForever(autoreverses: true), {
+                        animating.toggle()
+                    })
+                }
+                .onDisappear {
+                    onHide = true
+                    counter.reset()
+                }
+        }
     }
 }
 
-struct PortalLoader_Previews: PreviewProvider {
+struct LoadingAnimationView_Previews: PreviewProvider {
     static var previews: some View {
-        PortalLoader()
+        LoadingAnimationView()
     }
 }
