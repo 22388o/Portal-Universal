04a3d50ad7c1a445507caac00575774a0652eaa6
cuhte3 <sintezzlab@gmail.com>
2021-07-22 17:52:50 +0300

    Update KeychainStorage.swift

--- Shared/Services/Storage/KeychainStorage.swift
+++ Shared/Services/Storage/KeychainStorage.swift
@@ -13,23 +13,23 @@ final class KeychainStorage: IKeychainStorage {
     private let keychain: Keychain
         
     init(keychain: Keychain) {
-        self.keychain = keychain.accessibility(.whenUnlockedThisDeviceOnly)
+        self.keychain = keychain.accessibility(.whenPasscodeSetThisDeviceOnly)
     }
     
     func string(for key: String) -> String? {
         keychain[key]
     }
     
-    func save(string: String, key: UUID) {
-        keychain[key.uuidString] = string
+    func save(string: String, key: String) {
+        keychain[key] = string
     }
     
     func data(for key: String) -> Data? {
         keychain[data: key]
     }
     
-    func save(data: Data, key: UUID) {
-        keychain[data: key.uuidString] = data
+    func save(data: Data, key: String) {
+        keychain[data: key] = data
     }
     
     func clear() throws {
@@ -40,8 +40,39 @@ final class KeychainStorage: IKeychainStorage {
         try keychain.remove(key)
     }
     
-    func recoverStringArray(for key: UUID) -> [String]? {
-        guard let data = data(for: key.uuidString), let seed = data.toStringArray else { return nil }
+    func recoverStringArray(for key: String) -> [String]? {
+        guard let data = data(for: key), let seed = data.toStringArray else { return nil }
         return seed
+    }
+    
+    func value<T: LosslessStringConvertible>(for key: String) -> T? {
+        guard let string = keychain[key] else {
+            return nil
+        }
+        return T(string)
+    }
+
+    func set<T: LosslessStringConvertible>(value: T?, for key: String) throws {
+        if let value = value {
+            try keychain.set(value.description, key: key)
+        } else {
+            try keychain.remove(key)
+        }
+    }
+
+    func value(for key: String) -> Data? {
+        try? keychain.getData(key)
+    }
+
+    func set(value: Data?, for key: String) throws {
+        if let value = value {
+            try keychain.set(value, key: key)
+        } else {
+            try keychain.remove(key)
+        }
+    }
+
+    func removeValue(for key: String) throws {
+        try keychain.remove(key)
     }
 }
