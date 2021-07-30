5831f4acc819ad2cc60fb5eea91789a2ee415475
cuhte3 <sintezzlab@gmail.com>
2021-07-30 13:55:42 +0300

    Update CoinManager.swift

--- Shared/Services/Manager/CoinManager.swift
+++ Shared/Services/Manager/CoinManager.swift
@@ -7,16 +7,31 @@
 
 import Foundation
 import SwiftUI
-
-protocol ICoinManager {
-    var coins: [Coin] { get }
-}
+import Combine
 
 class CoinManager: ICoinManager {
-    var coins: [Coin] {
-        [
-            Coin(type: .bitcoin, code: "BTC", name: "Bitcoin", decimal: 18, color: Color.green, icon: Image("iconBtc")),
-            Coin(type: .ethereum, code: "ETH", name: "Ethereum", decimal: 18, color: Color.blue, icon: Image("iconEth"))
-        ]
+    var onCoinsUpdatePublisher = PassthroughSubject<[Coin], Never>()
+    
+    private var storage: ICoinStorage
+    private var subscriptions = Set<AnyCancellable>()
+    var coins: [Coin] = []
+    
+    init(storage: ICoinStorage) {
+        self.storage = storage
+        
+        storage.onCoinsUpdatePublisher
+            .sink { [weak self] coins in
+                guard let self = self else { return }
+                self.coins =
+                [
+                    Coin(type: .bitcoin, code: "BTC", name: "Bitcoin", decimal: 18, color: Color.green, icon: Image("iconBtc")),
+                    Coin(type: .ethereum, code: "ETH", name: "Ethereum", decimal: 18, color: Color.blue, icon: Image("iconEth"))
+                ]
+                
+                + coins
+                
+                self.onCoinsUpdatePublisher.send(self.coins)
+            }
+            .store(in: &subscriptions)
     }
 }
