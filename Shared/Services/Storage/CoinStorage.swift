c280ae6eed8e73f1d3b235c898f1ab6603d03920
cuhte3 <sintezzlab@gmail.com>
2021-08-02 21:43:49 +0300

    coin storage: fetching server erc20 tokens list by default instead of using local stored file

--- Shared/Services/Storage/CoinStorage.swift
+++ Shared/Services/Storage/CoinStorage.swift
@@ -9,15 +9,17 @@ import Foundation
 import Combine
 import Coinpaprika
 
-final class CoinStorage: ICoinStorage {
+final class CoinStorage: ICoinStorage, ObservableObject {
     var onCoinsUpdatePublisher = PassthroughSubject<[Coin], Never>()
-    
     var coins: [Coin] = []
     
     private var tokens: [Erc20TokenCodable] = []
     
     private let jsonDecoder: JSONDecoder
+    private let marketData: MarketDataRepository
     private var subscriptions = Set<AnyCancellable>()
+    
+    @Published private var tokensReady: Bool = false
     
     private var url: URL? {
         URL(string: "https://cryptomarket-api.herokuapp.com/erc20_token_list")
@@ -25,20 +27,21 @@ final class CoinStorage: ICoinStorage {
     
     init(marketData: MarketDataRepository, jsonDecoder: JSONDecoder = JSONDecoder()) {
         self.jsonDecoder = jsonDecoder
+        self.marketData = marketData
         
-        loadTokensData()
+        updateErc20TokensData()
         
-        marketData.$tickersReady
+        marketData.$tickersReady.combineLatest($tokensReady)
             .receive(on: DispatchQueue.global())
-            .sink { [weak self] ready in
+            .sink { [weak self] _tickersReady, _tokensReady  in
                 guard let self = self else { return }
-                if ready {
+                if _tickersReady && _tokensReady {
                     self.coins = self.tokens.compactMap {
                         Coin(type: .erc20(address: $0.contractAddress), code: $0.symbol, name: $0.name, decimal: $0.decimal, iconUrl: $0.iconURL)
                     }
                     .filter{ marketData.ticker(coin: $0) != nil }
                     .sorted(by: { $0.code < $1.code })
-                    
+
                     self.onCoinsUpdatePublisher.send(self.coins)
                 }
             }
@@ -50,11 +53,7 @@ final class CoinStorage: ICoinStorage {
         guard let allTokens = try? jsonDecoder.decode([String : Erc20TokenCodable].self, from: data) else { return }
         
         tokens = allTokens.map{ $0.value }.filter { !$0.iconURL.isEmpty }
-        
-//        coins = tokens.compactMap {
-//            Coin(type: .erc20(address: $0.contractAddress), code: $0.symbol, name: $0.name, decimal: $0.decimal)
-//        }
-//        .sorted(by: { $0.code < $1.code })
+        tokensReady = true
     }
     
     
@@ -62,15 +61,18 @@ final class CoinStorage: ICoinStorage {
         guard let url = self.url else { return }
         
         URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .userInitiated))
             .tryMap { $0.data }
             .decode(type: [String : Erc20TokenCodable].self, decoder: jsonDecoder)
-            .sink { (completion) in
+            .sink { completion in
                 if case let .failure(error) = completion {
                     print(error.localizedDescription)
                 }
-            } receiveValue: { erc20Tokens in
+            } receiveValue: { [weak self] erc20Tokens in
+                guard let self = self else { return }
                 let valildTokens = erc20Tokens.map{ $0.value }.filter { !$0.iconURL.isEmpty }
-                print(valildTokens.count)
+                self.tokens = valildTokens
+                self.tokensReady = !self.tokens.isEmpty
             }
             .store(in: &subscriptions)
     }
