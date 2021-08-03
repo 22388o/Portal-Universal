05e719bca916448aecf50c40b9594219db6082b2
cuhte3 <sintezzlab@gmail.com>
2021-08-03 15:22:16 +0300

    coin storage improvements

--- Shared/Services/Storage/CoinStorage.swift
+++ Shared/Services/Storage/CoinStorage.swift
@@ -9,71 +9,45 @@ import Foundation
 import Combine
 import Coinpaprika
 
-final class CoinStorage: ICoinStorage, ObservableObject {
+final class CoinStorage: ICoinStorage {
     var onCoinsUpdatePublisher = PassthroughSubject<[Coin], Never>()
+    
     var coins: [Coin] = []
     
-    private var tokens: [Erc20TokenCodable] = []
-    
-    private let jsonDecoder: JSONDecoder
-    private let marketData: MarketDataRepository
     private var subscriptions = Set<AnyCancellable>()
+    private var updater: ERC20TokensUpdater
+    private var marketData: MarketDataRepository
     
-    @Published private var tokensReady: Bool = false
-    
-    private var url: URL? {
-        URL(string: "https://cryptomarket-api.herokuapp.com/erc20_token_list")
-    }
-    
-    init(marketData: MarketDataRepository, jsonDecoder: JSONDecoder = JSONDecoder()) {
-        self.jsonDecoder = jsonDecoder
+    init(updater: ERC20TokensUpdater, marketData: MarketDataRepository) {
+        self.updater = updater
         self.marketData = marketData
         
-        updateErc20TokensData()
-        
-        marketData.$tickersReady.combineLatest($tokensReady)
-            .receive(on: DispatchQueue.global())
-            .sink { [weak self] _tickersReady, _tokensReady  in
+        subscribe()
+    }
+    
+    private func subscribe() {
+        updater.onTokensUpdatePublisher.combineLatest(marketData.$tickersReady)
+            .sink { [weak self] tokens, ready in
                 guard let self = self else { return }
-                if _tickersReady && _tokensReady {
-                    self.coins = self.tokens.compactMap {
-                        Coin(type: .erc20(address: $0.contractAddress), code: $0.symbol, name: $0.name, decimal: $0.decimal, iconUrl: $0.iconURL)
-                    }
-                    .filter{ marketData.ticker(coin: $0) != nil }
+                if ready && !tokens.isEmpty {
+                    let coins = tokens.compactMap {
+                        Coin(
+                            type: .erc20(address: $0.contractAddress),
+                            code: $0.symbol,
+                            name: $0.name,
+                            decimal: $0.decimal,
+                            iconUrl: $0.iconURL
+                        )
+                    }.filter { self.marketData.ticker(coin: $0) != nil }
                     .sorted(by: { $0.code < $1.code })
-
-                    self.onCoinsUpdatePublisher.send(self.coins)
+                    
+                    self.coins = coins
+                    self.onCoinsUpdatePublisher.send(coins)
                 }
-            }
-            .store(in: &subscriptions)
-    }
-    
-    private func loadTokensData() {
-        guard let data = erc20JSON.data(using: .utf8) else { return }
-        guard let allTokens = try? jsonDecoder.decode([String : Erc20TokenCodable].self, from: data) else { return }
-        
-        tokens = allTokens.map{ $0.value }.filter { !$0.iconURL.isEmpty }
-        tokensReady = true
-    }
-    
-    
-    private func updateErc20TokensData() {
-        guard let url = self.url else { return }
-        
-        URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .userInitiated))
-            .tryMap { $0.data }
-            .decode(type: [String : Erc20TokenCodable].self, decoder: jsonDecoder)
-            .sink { completion in
-                if case let .failure(error) = completion {
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] erc20Tokens in
-                guard let self = self else { return }
-                let valildTokens = erc20Tokens.map{ $0.value }.filter { !$0.iconURL.isEmpty }
-                self.tokens = valildTokens
-                self.tokensReady = !self.tokens.isEmpty
             }
             .store(in: &subscriptions)
     }
 }
+ 
+
+
