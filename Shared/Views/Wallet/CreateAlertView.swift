794b489d03f716dfb288353c074a71ea5a203ee2
cuhte3 <sintezzlab@gmail.com>
2021-08-26 12:24:07 +0300

    unused core removed

--- Shared/Views/Wallet/CreateAlertView.swift
+++ Shared/Views/Wallet/CreateAlertView.swift
@@ -187,28 +187,3 @@ struct CreateAlertView_Previews: PreviewProvider {
         CreateAlertView(asset: Asset.bitcoin(), presented: .constant(true))
     }
 }
-
-extension Bool {
-     static var iOS: Bool {
-        #if os(iOS)
-        return true
-        #else
-        return false
-        #endif
-     }
- }
-
-extension View {
-    /// Applies the given transform if the given condition evaluates to `true`.
-    /// - Parameters:
-    ///   - condition: The condition to evaluate.
-    ///   - transform: The transform to apply to the source `View`.
-    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-}
