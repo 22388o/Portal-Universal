73cb30f2ccbd5827c29bbed18cd2a1f49e64a3ef
cuhte3 <sintezzlab@gmail.com>
2021-09-27 13:15:24 +0300

    Create ExchangeBalanceInfo.swift

---
+++ Shared/Models/ExchangeBalanceInfo.swift
@@ -0,0 +1,47 @@
+//
+//  ExchangeBalanceInfo.swift
+//  Portal
+//
+//  Created by Farid on 27.09.2021.
+//
+
+import Foundation
+
+struct ExchangeBalanceInfo: Decodable {
+    let makerCommission: Int
+    let takerCommission: Int
+    let buyerCommission: Int
+    let sellerCommission: Int
+    let updateTime: Int
+    let canTrade: Bool
+    let canWithdraw: Bool
+    let canDeposit: Bool
+    var balances: [ExchangeBalanceModel]
+    
+    enum Keys: String, CodingKey {
+        case makerCommission
+        case takerCommission
+        case buyerCommission
+        case sellerCommission
+        case updateTime
+        case canTrade
+        case canWithdraw
+        case canDeposit
+        case balances
+    }
+}
+
+extension ExchangeBalanceInfo {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+        makerCommission = try container.decode(Int.self, forKey: .makerCommission)
+        takerCommission = try container.decode(Int.self, forKey: .takerCommission)
+        buyerCommission = try container.decode(Int.self, forKey: .buyerCommission)
+        sellerCommission = try container.decode(Int.self, forKey: .sellerCommission)
+        updateTime = try container.decode(Int.self, forKey: .updateTime)
+        canTrade =  try container.decode(Bool.self, forKey: .canTrade)
+        canWithdraw = try container.decode(Bool.self, forKey: .canWithdraw)
+        canDeposit = try container.decode(Bool.self, forKey: .canDeposit)
+        balances = try container.decode([ExchangeBalanceModel].self, forKey: .balances).filter{$0.free != "0.00000000"}
+    }
+}
