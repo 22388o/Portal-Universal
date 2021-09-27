5806de05ba6f169db424bcd8cb6d0934fc7e2bc2
cuhte3 <sintezzlab@gmail.com>
2021-09-27 13:39:09 +0300

    Create ExchangeDataUpdater.swift

---
+++ Shared/Services/Updaters/ExchangeDataUpdater.swift
@@ -0,0 +1,65 @@
+//
+//  ExchangeDataUpdater.swift
+//  Portal
+//
+//  Created by Farid on 09.09.2021.
+//
+
+import Foundation
+import Combine
+
+final class ExchangeDataUpdater {
+    let onExchangesUpdatePublisher = PassthroughSubject<[ExchangeModel], Never>()
+    let onTraidingPairsUpdatePublisher = PassthroughSubject<[TradingPairModel], Never>()
+    
+    private let baseUrl: String = "https://cryptomarket-api.herokuapp.com/"
+    private let supportedExchangesIds: String = "binance,coinbasepro,kraken"
+    private let jsonDecoder: JSONDecoder
+    private var subscriptions = Set<AnyCancellable>()
+    
+    private var supportedExchangesUrl: URL? {
+        URL(string: "\(baseUrl)exchange/list")
+    }
+    
+    private var traidingPairsUrl: URL? {
+        URL(string: "\(baseUrl)exchange/pairs?ex=\(supportedExchangesIds)")
+    }
+    
+    init(jsonDecoder: JSONDecoder = JSONDecoder()) {
+        self.jsonDecoder = jsonDecoder
+        updateSupportedExchanges()
+        updateTraidingPairs()
+    }
+    
+    private func updateSupportedExchanges() {
+        guard let url = self.supportedExchangesUrl else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { $0.data }
+            .decode(type: [ExchangeModel].self, decoder: jsonDecoder)
+            .sink { completion in
+                if case let .failure(error) = completion {
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] exchanges in
+                self?.onExchangesUpdatePublisher.send(exchanges)
+            }
+            .store(in: &subscriptions)
+    }
+    
+    private func updateTraidingPairs() {
+        guard let url = self.traidingPairsUrl else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { $0.data }
+            .decode(type: [TradingPairModel].self, decoder: jsonDecoder)
+            .sink { completion in
+                if case let .failure(error) = completion {
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] traidingPairs in
+                self?.onTraidingPairsUpdatePublisher.send(traidingPairs)
+            }
+            .store(in: &subscriptions)
+    }
+}
