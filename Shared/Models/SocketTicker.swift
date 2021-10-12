614b23b3829b9d34e03aa021fb34eaf7871a0011
cuhte3 <sintezzlab@gmail.com>
2021-10-12 17:28:23 +0300

    socket ticker number formatter

--- Shared/Models/SocketTicker.swift
+++ Shared/Models/SocketTicker.swift
@@ -35,10 +35,22 @@ struct SocketTicker: Equatable {
             return
         }
         
-        change = String(format: "%.8f", Double(changeString) ?? "-")
-        last = String(format: "%.8f", Double(lastString) ?? "-")
-        high = String(format: "%.8f", Double(highestString) ?? "-")
-        low = String(format: "%.8f", Double(lowestString) ?? "-")
+        if let changeDouble = Double(changeString), let lastDouble = Double(lastString), let highDouble = Double(highestString), let lowDouble = Double(lowestString) {
+            let changeFormat = StringFormatter.format(number: changeDouble)
+            let lastFormat = StringFormatter.format(number: lastDouble)
+            let highFormat = StringFormatter.format(number: highDouble)
+            let lowFormat = StringFormatter.format(number: lowDouble)
+            
+            change = String(format: changeFormat, changeDouble)
+            last = String(format: lastFormat, lastDouble)
+            high = String(format: highFormat, highDouble)
+            low = String(format: lowFormat, lowDouble)
+        } else {
+            change = String(format: "%.8f", Double(changeString) ?? "-")
+            last = String(format: "%.8f", Double(lastString) ?? "-")
+            high = String(format: "%.8f", Double(highestString) ?? "-")
+            low = String(format: "%.8f", Double(lowestString) ?? "-")
+        }
         
         if let quoteVolume = state["quoteVolume"] as? String {
             volume = String(format: "%.3f", Double(quoteVolume) ?? 0.0) + " \(_quote)"
