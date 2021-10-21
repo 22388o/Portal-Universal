45524f942f1a94efbb7704989669412bc9a249b7
cuhte3 <sintezzlab@gmail.com>
2021-10-21 15:41:00 +0300

    Local storage cache storing/fetching

--- Shared/Services/Storage/DBLocalStorage.swift
+++ Shared/Services/Storage/DBLocalStorage.swift
@@ -7,21 +7,71 @@
 
 import Foundation
 import CoreData
+import Combine
 
 final class DBlocalStorage {
     var context: NSManagedObjectContext
+    private var cache: DBCache?
+    
+    var tickers: [Ticker] = []
+    var fiatCurrencies: [FiatCurrency] = []
     
     init(context: NSManagedObjectContext) {
         self.context = context
+        loadCache()
     }
     
     func delete(account: AccountRecord) throws {
-        context.delete(account)
+        context.performAndWait {
+            context.delete(account)
+        }
         do {
             try context.save()
         } catch {
             throw DBStorageError.cannotSaveContext(error: error)
         }
+    }
+    
+    private func loadCache() {
+        context.performAndWait {
+            let request = DBCache.fetchRequest() as NSFetchRequest<DBCache>
+            
+            do {
+                self.cache = try context.fetch(request).first
+                
+                guard cache == nil else {
+                    print("Cache fetched")
+                    
+                    tickers = fetchCachedTickers()
+                    fiatCurrencies = fetchCachedFiatCurrencies()
+                    
+                    return
+                }
+                
+                print("Creating cache db entity...")
+                let cache = DBCache(context: context)
+                context.insert(cache)
+                try context.save()
+                
+                self.cache = cache
+            } catch {
+                print("Loading cache error: \(error)")
+            }
+        }
+    }
+    
+    fileprivate func fetchCachedTickers() -> [Ticker] {
+        guard let cache = self.cache, let tickersData = cache.tickersData else { return [] }
+        let tickers = try? JSONDecoder().decode([Ticker].self, from: tickersData)
+        print("Cached tickers fetched")
+        return tickers ?? []
+    }
+    
+    fileprivate func fetchCachedFiatCurrencies() -> [FiatCurrency] {
+        guard let cache = self.cache, let fiatCUrrenciesData = cache.fiatCurrenciesData else { return [] }
+        let currencies = try? JSONDecoder().decode([FiatCurrency].self, from: fiatCUrrenciesData)
+        print("Cached fiat currencies fetched")
+        return currencies ?? []
     }
 }
 
@@ -58,8 +108,10 @@ extension DBlocalStorage: IAccountStorage {
     }
     
     func save(accountRecord: AccountRecord) {
-        context.insert(accountRecord)
-        try? context.save()
+        context.performAndWait {
+            context.insert(accountRecord)
+            try? context.save()
+        }
     }
     
     func deleteAccountRecord(by id: String) {
@@ -68,5 +120,39 @@ extension DBlocalStorage: IAccountStorage {
     
     func deleteAllAccountRecords() {
         
+    }
+}
+
+import Coinpaprika
+
+extension DBlocalStorage: IDBCacheStorage {
+    func store(tickers: [Ticker]?) {
+        guard let tickers = tickers else { return }
+
+        context.performAndWait {
+            do {
+                let tickersData = try JSONEncoder().encode(tickers)
+                cache?.tickersData = tickersData
+                try context.save()
+                print("Tickers updated")
+            } catch {
+                print("Encoding error: \(error)")
+            }
+        }
+        self.tickers = tickers
+    }
+    
+    func store(fiatCurrencies: [FiatCurrency]) {
+        context.performAndWait {
+            do {
+                let currenciesData = try JSONEncoder().encode(fiatCurrencies)
+                cache?.fiatCurrenciesData = currenciesData
+                try context.save()
+                print("Fiat currencies updated")
+            } catch {
+                print("Encoding error: \(error)")
+            }
+        }
+        self.fiatCurrencies = fiatCurrencies
     }
 }
