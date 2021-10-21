3be105099b8fb940522c35375b2ac5a534d2d3e8
cuhte3 <sintezzlab@gmail.com>
2021-10-21 15:33:59 +0300

    IDBCacheStorageProtocol added

---
+++ Shared/Protocols/IDBCacheStorage.swift
@@ -0,0 +1,17 @@
+//
+//  IDBCacheStorage.swift
+//  Portal
+//
+//  Created by Farid on 21.10.2021.
+//
+
+import Coinpaprika
+
+protocol IDBCacheStorage {
+    var context: NSManagedObjectContext { get }
+    var tickers: [Ticker] { get }
+    var fiatCurrencies: [FiatCurrency] { get }
+    
+    func store(fiatCurrencies: [FiatCurrency])
+    func store(tickers: [Ticker]?)
+}
