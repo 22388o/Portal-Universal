89220da2ff5ec10590ac1a86dc4dc4fb6b0887b7
cuhte3 <sintezzlab@gmail.com>
2021-11-11 16:17:06 +0300

    LineChartRepresentable supports marker for macOS

--- macOS/Components/LineChartRepresentable.swift
+++ macOS/Components/LineChartRepresentable.swift
@@ -10,32 +10,57 @@ import Charts
 
 struct LineChartRepresentable: NSViewRepresentable {
     let chartDataEntries: [ChartDataEntry]
+
+    class Coordinator: NSObject, ChartViewDelegate {
+        var lineChartRepresentable: LineChartRepresentable
+        var lineChartView: LineChartView
+        var chartDataEntries: [ChartDataEntry]?
+
+        init(_ lineChartRepresentable: LineChartRepresentable) {
+            self.lineChartRepresentable = lineChartRepresentable
+            self.lineChartView = LineChartView()
+            self.lineChartView.applyStandardSettings()
+            super.init()
+            self.lineChartView.delegate = self
+        }
+
+        func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                chartView.clear()
+                self.updateChartData()
+            }
+        }
+
+        func updateChartData() {
+            let data = LineChartData()
+            
+            if let chartDataEntries = self.chartDataEntries {
+                let dataSet = chartDataEntries.dataSet()
+
+                data.dataSets = [dataSet]
+
+                let maxValue = chartDataEntries.map{$0.y}.max()
+
+                if maxValue != nil {
+                    dataSet.gradientPositions = [0, CGFloat(maxValue!)]
+                    lineChartView.data = data
+                    lineChartView.notifyDataSetChanged()
+                }
+            }
+        }
+    }
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     func makeNSView(context: Context) -> LineChartView {
-        let lineChart = LineChartView()
-        lineChart.applyStandardSettings()
-                
-        updateChartData(lineChart: lineChart)
-        
-        return lineChart
+        context.coordinator.updateChartData()
+        return context.coordinator.lineChartView
     }
 
     func updateNSView(_ lineChart: LineChartView, context: Context) {
-        updateChartData(lineChart: lineChart)
-    }
-    
-    func updateChartData(lineChart: LineChartView) {
-        let data = LineChartData()
-        let dataSet = chartDataEntries.dataSet()
-        
-        data.dataSets = [dataSet]
-                
-        let maxValue = chartDataEntries.map{$0.y}.max()
-        
-        if maxValue != nil {
-//            dataSet.gradientPositions = [0, CGFloat(maxValue!)]
-            lineChart.data = data
-            lineChart.notifyDataSetChanged()
-        }
+        context.coordinator.chartDataEntries = chartDataEntries
+        context.coordinator.updateChartData()
     }
 }
