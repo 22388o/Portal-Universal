70385cdc3fa086084e1c6b97a973a271c11a52f7
cuhte3 <farid@getportal.co>
2021-12-07 01:05:12 +0300

    price alert model

---
+++ Shared/Models/PriceAlertModel.swift
@@ -0,0 +1,17 @@
+//
+//  PriceAlertModel.swift
+//  Portal
+//
+//  Created by farid on 12/6/21.
+//
+
+import Foundation
+
+struct PriceAlertModel {
+    let id: String
+    let coin: String
+    let timestamp: String
+    let title: String
+    let value: String
+}
+
