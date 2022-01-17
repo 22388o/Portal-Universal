1920d3dde1a66c06e7b3237896510b42a69adfae
cuhte3 <farid@getportal.co>
2022-01-17 19:30:40 +0300

    fixing commit
    - fixes FabricLabs/Portal#365

--- UnitTestsMacOS/AssetViewModelUnitTests.swift
+++ UnitTestsMacOS/AssetViewModelUnitTests.swift
@@ -35,7 +35,7 @@ class AssetViewModelUnitTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sut = nil
-        subscriptions = []
+        subscriptions.removeAll()
     }
     
     func testDefaultValues() throws {
