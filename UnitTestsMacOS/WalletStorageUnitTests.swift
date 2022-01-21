2a134f47c4d7015ae5d7d51c3150aaf32f0cceb7
cuhte3 <farid@getportal.co>
2022-01-21 19:25:39 +0300

    WalletStorageUnitTests

---
+++ UnitTestsMacOS/WalletStorageUnitTests.swift
@@ -0,0 +1,90 @@
+//
+//  WalletStorageUnitTests.swift
+//  UnitTestsMacOS
+//
+//  Created by farid on 1/21/22.
+//
+
+import XCTest
+@testable import Portal
+
+import Combine
+
+class MockedCoinMannager: ICoinManager {
+    var onCoinsUpdate = PassthroughSubject<[Coin], Never>()
+    var coins: [Coin] = []
+    
+    func addCoins() {
+        coins = [.bitcoin(), .ethereum()]
+    }
+}
+
+class WalletStorageUnitTests: XCTestCase {
+    
+    private var sut: WalletStorage!
+    private var subscriptions = Set<AnyCancellable>()
+    private let accountMannager: IAccountManager = MockedAccountManager()
+    private var coinMannager = MockedCoinMannager()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = WalletStorage(coinManager: coinMannager, accountManager: accountMannager)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func testOnActiveAccountUpdateUpdatesWallets() throws {
+        XCTAssertEqual(sut.wallets.count, 0)
+        
+        coinMannager.addCoins()
+        
+        let promisse = expectation(description: "wallets updated")
+        
+        sut.onWalletsUpdate.sink(receiveValue: { wallets in
+            XCTAssertNotNil(wallets, "wallets are nil")
+            promisse.fulfill()
+        })
+        .store(in: &subscriptions)
+        
+        accountMannager.onActiveAccountUpdate.send(nil)
+        
+        wait(for: [promisse], timeout: 0.2)
+        
+        XCTAssertEqual(sut.wallets.count, 2)
+    }
+    
+    func testOnCoinsUpdateUpdatesWallets() throws {
+        XCTAssertEqual(sut.wallets.count, 0)
+
+        coinMannager.addCoins()
+        
+        let promisse = expectation(description: "wallets updated")
+        
+        sut.onWalletsUpdate.sink(receiveValue: { wallets in
+            XCTAssertNotNil(wallets, "wallets are nil")
+            promisse.fulfill()
+        })
+        .store(in: &subscriptions)
+        
+        coinMannager.onCoinsUpdate.send([])
+        
+        wait(for: [promisse], timeout: 0.2)
+        
+        XCTAssertEqual(sut.wallets.count, 2)
+    }
+    
+    func testClearWallets() throws {
+        coinMannager.addCoins()
+        
+        sut = WalletStorage(coinManager: coinMannager, accountManager: accountMannager)
+        
+        XCTAssertEqual(sut.wallets.count, 2)
+        
+        sut.clearWallets()
+        
+        XCTAssertEqual(sut.wallets.count, 0)
+    }
+}
