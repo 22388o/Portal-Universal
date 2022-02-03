8e5e66b53dac481c832a2cf12bf0191faa2ba755
cuhte3 <farid@getportal.co>
2022-02-03 16:02:20 +0300

    AssetItemView uses Button functionality under the hood to being able generate click events in UI Tests

--- Shared/Views/Wallet/Asset/AssetItemView.swift
+++ Shared/Views/Wallet/Asset/AssetItemView.swift
@@ -17,54 +17,57 @@ struct AssetItemView: View {
     }
         
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 5)
-                .fill(viewModel.selected ? Color.white : Color.clear)
-            
+        Button {
+            onTap()
+        } label: {
             ZStack {
-                HStack {
-                    HStack(spacing: 12) {
-                        ZStack {
-                            if viewModel.adapterState != .synced {
-                                CircularProgressBar(progress: $viewModel.syncProgress)
-                                    .frame(width: 26, height: 26)
-                            }
-                            CoinImageView(size: 24, url: viewModel.coin.icon)
-                        }
-                        .frame(width: 26, height: 26)
-                        
-                        if viewModel.adapterState != .synced {
-                            VStack(alignment: .leading) {
-                                Text(viewModel.balanceString)
-                                if !viewModel.syncProgressString.isEmpty {
-                                    Text(viewModel.syncProgressString)
-                                        .font(.mainFont(size: 14))
-                                        .foregroundColor(viewModel.selected ? .gray : Color(red: 160/255, green: 190/255, blue: 186/255, opacity: 1))
-                                }
-                            }
-                        } else {
-                            Text(viewModel.balanceString)
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    Text(viewModel.changeString)
-                        .foregroundColor(viewModel.changeLabelColor)
-                }
-                .padding(.horizontal, 20)
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(viewModel.selected ? Color.white : Color.clear)
                 
-                Text(viewModel.totalValueString)
-                    .transition(.identity)
-            }
-            .font(.mainFont(size: 18))
-            .foregroundColor(viewModel.selected ? .black : Color(red: 160/255, green: 190/255, blue: 186/255, opacity: 1))
-            .contentShape(Rectangle())
-            .onTapGesture {
-                onTap()
+                ZStack {
+                    HStack {
+                        HStack(spacing: 12) {
+                            ZStack {
+                                if viewModel.adapterState != .synced {
+                                    CircularProgressBar(progress: $viewModel.syncProgress)
+                                        .frame(width: 26, height: 26)
+                                }
+                                CoinImageView(size: 24, url: viewModel.coin.icon)
+                            }
+                            .frame(width: 26, height: 26)
+                            
+                            if viewModel.adapterState != .synced {
+                                VStack(alignment: .leading) {
+                                    Text(viewModel.balanceString)
+                                    if !viewModel.syncProgressString.isEmpty {
+                                        Text(viewModel.syncProgressString)
+                                            .font(.mainFont(size: 14))
+                                            .foregroundColor(viewModel.selected ? .gray : Color(red: 160/255, green: 190/255, blue: 186/255, opacity: 1))
+                                    }
+                                }
+                            } else {
+                                Text(viewModel.balanceString)
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        Text(viewModel.changeString)
+                            .foregroundColor(viewModel.changeLabelColor)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Text(viewModel.totalValueString)
+                        .transition(.identity)
+                }
+                .font(.mainFont(size: 18))
+                .foregroundColor(viewModel.selected ? .black : Color(red: 160/255, green: 190/255, blue: 186/255, opacity: 1))
+                .contentShape(Rectangle())
             }
         }
         .frame(height: 64)
+        .buttonStyle(PlainButtonStyle())
+        .identifier(viewModel.coin.code + " Asset Button Identifier")
     }
 }
 
