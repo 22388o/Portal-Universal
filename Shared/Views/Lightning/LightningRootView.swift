e5ae0961c8a43b1fb9a3956aad09e5c786c50938
cuhte3 <farid@getportal.co>
2022-06-02 16:23:22 +0400

    LightningRootView added

---
+++ Shared/Views/Lightning/LightningRootView.swift
@@ -0,0 +1,20 @@
+//
+//  LightningRootView.swift
+//  Portal
+//
+//  Created by farid on 6/2/22.
+//
+
+import SwiftUI
+
+struct LightningRootView: View {
+    var body: some View {
+        Text("Lightning Root View")
+    }
+}
+
+struct LightningRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightningRootView()
+    }
+}
