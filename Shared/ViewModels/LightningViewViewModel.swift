c993126ed8bc2cd41c0628b4e255f2a7ce2670ea
cuhte3 <farid@getportal.co>
2022-06-06 22:11:48 +0700

    Create LightningViewViewModel.swift

---
+++ Shared/ViewModels/LightningViewViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  LightningViewViewModel.swift
+//  Portal
+//
+//  Created by farid on 6/6/22.
+//
+
+import Foundation
+
+class LightningViewViewModel: ObservableObject {
+    
+    enum ViewState {
+        case root, manage, openChannel, createInvoice, fundChannel
+    }
+    
+    @Published var createInvoice: Bool = false
+    @Published var payInvoice: Bool = false
+    @Published var manageChannels: Bool = false
+    @Published var showActivityDetails: Bool = false
+    @Published var payments: [LightningPayment] = []
+    @Published var viewState: ViewState = .root
+    
+    var btcAdapter: IAdapter
+//    var channelManager = PolarConnectionExperiment.shared.service!.manager.channelManager
+    var selectedPayment: LightningPayment?
+    
+    var onChainBalanceString: String {
+        "0.000202" + " BTC"
+    }
+    
+    var channelsBalanceString: String {
+//        var balaance: UInt64 = 0
+//        for channel in channelManager.list_usable_channels() {
+//            balaance+=channel.get_balance_msat()/1000
+//        }
+        return "\(20000) sat"
+    }
+    
+    init(adapter: IAdapter) {
+        self.btcAdapter = adapter
+    }
+    
+    func refreshPayments() {
+        payments = [
+            LightningPayment(id: UUID().uuidString, satAmount: 1000, created: Date(), description: "Coffee", state: .sent),
+            LightningPayment(id: UUID().uuidString, satAmount: 3000, created: Date(), description: "Dinner", state: .recieved),
+            LightningPayment(id: UUID().uuidString, satAmount: 5000, created: Date(), description: "Test2", state: .requested),
+            LightningPayment(id: UUID().uuidString, satAmount: 2030, created: Date(), description: "Test1", state: .recieved),
+            LightningPayment(id: UUID().uuidString, satAmount: 5000, created: Date(), description: "Test", state: .requested)
+        ]
+    }
+}
+
+extension LightningViewViewModel {
+    static func config() -> LightningViewViewModel {
+        let btcAdapter = Portal.shared.adapterManager.adapter(for: .bitcoin())
+        return LightningViewViewModel(adapter: btcAdapter!)
+    }
+}
