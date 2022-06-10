8d4d10e28562832f1d5248aa6b29c07e1bd0fa49
cuhte3 <farid@getportal.co>
2022-06-10 15:20:28 +0700

    Update LightningRootView.swift

--- Shared/Views/Lightning/LightningRootView.swift
+++ Shared/Views/Lightning/LightningRootView.swift
@@ -8,133 +8,31 @@
 import SwiftUI
 
 struct LightningRootView: View {
-    @StateObject var vm = LightningViewViewModel.config()
+    @State private var state: ViewState
+    
+    init(state: LightningRootView.ViewState = .root) {
+        _state = State(initialValue: state)
+    }
     
     var body: some View {
         ModalViewContainer(size: CGSize(width: 500, height: 650), {
             ZStack(alignment: .top) {
                 Color.portalBackground.edgesIgnoringSafeArea(.all)
-                    .onAppear {
-                        vm.refreshPayments()
-                    }
                 
-                switch vm.viewState {
+                switch state {
                 case .root:
-                    VStack {
-                        HStack {
-                            Text("Channels")
-                                .font(.mainFont(size: 18))
-                                .foregroundColor(Color.white)
-                            Spacer()
-                            
-                            Text("Manage")
-                                .font(.mainFont(size: 14))
-                                .foregroundColor(Color.lightActiveLabel)
-                                .contentShape(Rectangle())
-                                .onTapGesture {
-                                    withAnimation {
-                                        vm.viewState = .manage
-                                    }
-                                }
-                        }
-                        .padding()
-                        
-                        VStack {
-                            HStack {
-                                Text("On-chain")
-                                    .font(.mainFont(size: 14))
-                                    .foregroundColor(Color.lightInactiveLabel)
-                                
-                                Spacer()
-                                
-                                Text(vm.onChainBalanceString)
-                                    .font(.mainFont(size: 14))
-                                    .foregroundColor(Color.lightActiveLabel)
-                            }
-                            
-                            HStack {
-                                Text("Lightning")
-                                    .font(.mainFont(size: 14))
-                                    .foregroundColor(Color.lightInactiveLabel)
-                                
-                                Spacer()
-                                
-                                Text(vm.channelsBalanceString)
-                                    .font(.mainFont(size: 14))
-                                    .foregroundColor(Color.lightActiveLabel)
-                            }
-                            
-                            HStack {
-                                Button("Receive") {
-                                    vm.viewState = .createInvoice
-                                }
-                                .modifier(PButtonEnabledStyle(enabled: .constant(true)))
-                                Button("Send") {
-                                    vm.payInvoice.toggle()
-                                }
-                                .modifier(PButtonEnabledStyle(enabled: .constant(true)))
-                            }
-                            .sheet(isPresented: $vm.createInvoice, onDismiss: {
-                                vm.refreshPayments()
-                            }, content: {
-                                //CreateInvoiceView()
-                            })
-                            .sheet(isPresented: $vm.payInvoice, onDismiss: {
-                                vm.refreshPayments()
-                            }, content: {
-                                //PayInvoiceView()
-                            })
-                        }
-                        .padding()
-                        
-                        HStack {
-                            Text("Payments")
-                                .font(.mainFont(size: 14))
-                                .foregroundColor(Color.white)
-                            Spacer()
-                        }
-                        .padding([.horizontal])
-                        
-                        Rectangle()
-                            .frame(height: 1)
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(.white)
-                            .padding(.horizontal)
-                        
-                        if !vm.payments.isEmpty {
-                            ScrollView {
-                                VStack(spacing: 0) {
-                                    ForEach(vm.payments) { payment in
-                                        LightningPaymentItemView(payment: payment)
-                                            .onTapGesture {
-                                                guard payment.state == .requested else { return }
-                                                vm.selectedPayment = payment
-                                                vm.showActivityDetails.toggle()
-                                            }
-                                    }
-                                    .frame(height: 80)
-                                    .padding(.horizontal)
-                                    .padding(.vertical, 6)
-                                    .sheet(isPresented: $vm.showActivityDetails) {
-                                        ///ActivityDetailsView(activity: vm.selectedPayment!)
-                                    }
-                                }
-                            }
-                        } else {
-                            Spacer()
-                            Text("There is no activity.")
-                                .font(.mainFont(size: 14))
-                                .foregroundColor(Color.lightActiveLabel)
-                            Spacer()
-                        }
-                    }
+                    LightningView(viewState: $state)
                 case .manage:
-                    ManageChannelsView(viewState: $vm.viewState)
+                    ManageChannelsView(viewState: $state)
                 case .openChannel:
-                    OpenNewChannelView(viewModel: ChannelsViewModel(), viewState: $vm.viewState)
-                case .createInvoice:
-                    CreateInvoiceView(viewState: $vm.viewState)
+                    OpenNewChannelView(viewState: $state)
+                case .receive:
+                    CreateInvoiceView(viewState: $state)
                 case .fundChannel:
+                    EmptyView()
+                case .send:
+                    EmptyView()
+                case .paymentDetails(let payment):
                     EmptyView()
                 }
             }
@@ -142,8 +40,21 @@ struct LightningRootView: View {
     }
 }
 
+extension LightningRootView {
+    enum ViewState {
+        case root, manage, openChannel, fundChannel, receive, send, paymentDetails(LightningPayment)
+    }
+}
+
 struct LightningRootView_Previews: PreviewProvider {
     static var previews: some View {
-        LightningRootView()
+        Group {
+            LightningRootView(state: .root)
+            LightningRootView(state: .manage)
+            LightningRootView(state: .openChannel)
+            LightningRootView(state: .fundChannel)
+            LightningRootView(state: .send)
+            LightningRootView(state: .receive)
+        }
     }
 }
