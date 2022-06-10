b7b93972e5bbcf5148d5750ecffdd368a8f7c38e
cuhte3 <farid@getportal.co>
2022-06-10 15:20:24 +0700

    Create LightningView.swift

---
+++ Shared/Views/Lightning/LightningView.swift
@@ -0,0 +1,121 @@
+//
+//  LightningView.swift
+//  Portal
+//
+//  Created by farid on 6/9/22.
+//
+
+import SwiftUI
+
+struct LightningView: View {
+    @Binding var viewState: LightningRootView.ViewState
+    @StateObject private var viewModel = LightningRootViewViewModel.config()
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Channels")
+                    .font(.mainFont(size: 18))
+                    .foregroundColor(Color.white)
+                Spacer()
+                
+                Text("Manage")
+                    .font(.mainFont(size: 14))
+                    .foregroundColor(Color.lightActiveLabel)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        withAnimation {
+                            viewState = .manage
+                        }
+                    }
+            }
+            .padding()
+            
+            VStack {
+                HStack {
+                    Text("On-chain")
+                        .font(.mainFont(size: 14))
+                        .foregroundColor(Color.lightInactiveLabel)
+                    
+                    Spacer()
+                    
+                    Text(viewModel.onChainBalanceString)
+                        .font(.mainFont(size: 14))
+                        .foregroundColor(Color.lightActiveLabel)
+                }
+                
+                HStack {
+                    Text("Lightning")
+                        .font(.mainFont(size: 14))
+                        .foregroundColor(Color.lightInactiveLabel)
+                    
+                    Spacer()
+                    
+                    Text(viewModel.channelsBalanceString)
+                        .font(.mainFont(size: 14))
+                        .foregroundColor(Color.lightActiveLabel)
+                }
+                
+                HStack {
+                    Button("Receive") {
+                        viewState = .receive
+                    }
+                    .modifier(PButtonEnabledStyle(enabled: .constant(true)))
+                    Button("Send") {
+                        viewState = .send
+                    }
+                    .modifier(PButtonEnabledStyle(enabled: .constant(true)))
+                }
+            }
+            .padding()
+            
+            HStack {
+                Text("Payments")
+                    .font(.mainFont(size: 14))
+                    .foregroundColor(Color.white)
+                Spacer()
+            }
+            .padding([.horizontal])
+            
+            Rectangle()
+                .frame(height: 1)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.white)
+                .padding(.horizontal)
+            
+            if viewModel.payments.isEmpty {
+                Spacer()
+                Text("There is no payments.")
+                    .font(.mainFont(size: 14))
+                    .foregroundColor(Color.lightActiveLabel)
+                Spacer()
+            } else {
+                ScrollView {
+                    VStack(spacing: 0) {
+                        ForEach(viewModel.payments) { payment in
+                            LightningPaymentItemView(payment: payment)
+                                .onTapGesture {
+                                    guard payment.state == .requested else { return }
+                                    viewState = .paymentDetails(payment)
+                                }
+                        }
+                        .frame(height: 80)
+                        .padding(.horizontal)
+                        .padding(.vertical, 6)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            viewModel.refreshPayments()
+        }
+    }
+}
+
+struct LightningView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalViewContainer(size: CGSize(width: 500, height: 650), {
+            LightningView(viewState: .constant(.root))
+        })
+    }
+}
