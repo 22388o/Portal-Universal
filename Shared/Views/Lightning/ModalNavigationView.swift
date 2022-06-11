6364cd40c68ae6a22cf7fb8f6ada12e18abc0983
cuhte3 <farid@getportal.co>
2022-06-11 22:40:13 +0700

    Create ModalNavigationView.swift

---
+++ Shared/Views/Lightning/ModalNavigationView.swift
@@ -0,0 +1,43 @@
+//
+//  ModalNavigationView.swift
+//  Portal
+//
+//  Created by farid on 6/11/22.
+//
+
+import SwiftUI
+
+struct ModalNavigationView: View {
+    let title: String
+    let backButtonAction: (() -> Void)?
+    
+    var body: some View {
+        ZStack {
+            Text(title)
+                .font(.mainFont(size: 18))
+                .foregroundColor(Color.white)
+                .padding()
+            
+            HStack {
+                Text("Back")
+                    .foregroundColor(Color.lightActiveLabel)
+                    .font(.mainFont(size: 14))
+                    .padding()
+                    .onTapGesture {
+                        withAnimation {
+                            backButtonAction?()
+                        }
+                    }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ModalNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalNavigationView(title: "Test Navigation", backButtonAction: nil)
+            .padding()
+    }
+}
+
