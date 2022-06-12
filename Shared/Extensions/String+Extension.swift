c1c8e387fe7efb89ec906dd50940d42efe45d80f
cuhte3 <farid@getportal.co>
2022-06-12 16:04:41 +0700

    Update String+Extension.swift
    hexStringToBytes()

--- Shared/Extensions/String+Extension.swift
+++ Shared/Extensions/String+Extension.swift
@@ -11,4 +11,23 @@ extension String {
     func timestamp() -> TimeInterval {
         TimeInterval(exactly: self.doubleValue) ?? 0
     }
+    
+    func hexStringToBytes() -> [UInt8]? {
+        let hexStr = self.dropFirst(self.hasPrefix("0x") ? 2 : 0)
+
+        guard hexStr.count % 2 == 0 else { return nil }
+
+        var newData = [UInt8]()
+
+        var indexIsEven = true
+        for i in hexStr.indices {
+            if indexIsEven {
+                let byteRange = i...hexStr.index(after: i)
+                guard let byte = UInt8(hexStr[byteRange], radix: 16) else { return nil }
+                newData.append(byte)
+            }
+            indexIsEven.toggle()
+        }
+        return newData
+    }
 }
