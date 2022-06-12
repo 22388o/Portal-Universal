c88e1524930501a59b752c12919f4391ad70fb3f
cuhte3 <farid@getportal.co>
2022-06-12 15:25:28 +0700

    LightningPaymentDetailsViewModel

---
+++ Shared/ViewModels/LightningPaymentDetailsViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  LightningPaymentDetailsViewModel.swift
+//  Portal
+//
+//  Created by farid on 6/12/22.
+//
+
+import Combine
+import SwiftUI
+
+class LightningPaymentDetailsViewModel: ObservableObject {
+    let payment: LightningPayment
+    @Published var qrCode: UIImage?
+    
+    init(payment: LightningPayment) {
+        self.payment = payment
+        updateQRCode()
+    }
+    
+    private func qrCode(invoice: String?) -> UIImage {
+        guard let message = invoice?.data(using: .utf8) else { return UIImage() }
+        
+        let parameters: [String : Any] = [
+            "inputMessage": message,
+            "inputCorrectionLevel": "L"
+        ]
+        let filter = CIFilter(name: "CIQRCodeGenerator", parameters: parameters)
+        
+        guard let outputImage = filter?.outputImage else { return UIImage() }
+        
+        let scaledImage = outputImage.transformed(by: CGAffineTransform(scaleX: 6, y: 6))
+        guard let cgImage = CIContext().createCGImage(scaledImage, from: scaledImage.extent) else {
+            return UIImage()
+        }
+        
+        return UIImage(cgImage: cgImage, size: NSSize(width: 350, height: 350))
+    }
+    
+    private func updateQRCode() {
+        guard let invoice = payment.invoice else { return }
+        qrCode = qrCode(invoice: invoice)
+    }
+}
--- Shared/Views/Lightning/LightningPaymentDetailsView.swift
+++ Shared/Views/Lightning/LightningPaymentDetailsView.swift
@@ -9,13 +9,133 @@ import SwiftUI
 
 struct LightningPaymentDetailsView: View {
     @Binding var viewState: LightningRootView.ViewState
-    let payment: LightningPayment
+    @StateObject private var viewModel: LightningPaymentDetailsViewModel
+
+    init(viewState: Binding<LightningRootView.ViewState>, payment: LightningPayment) {
+        _viewState = viewState
+        _viewModel = StateObject(wrappedValue: LightningPaymentDetailsViewModel(payment: payment))
+    }
     
     var body: some View {
-        ModalNavigationView(title: "Payment Details", backButtonAction: {
-            viewState = .root
-        })
-        .padding()
+        ZStack(alignment: .top) {
+            Color.portalBackground.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ModalNavigationView(title: "Payment Details", backButtonAction: {
+                    viewState = .root
+                })
+                .padding()
+                
+                VStack {
+                    if let code = viewModel.qrCode {
+                        Image(nsImage: code)
+                            .resizable()
+                            .frame(width: 350, height: 350)
+                            .cornerRadius(10)
+                            .padding()
+                    }
+                    
+                    HStack {
+                        Text("Amount:")
+                            .font(.mainFont(size: 14))
+                            .foregroundColor(Color.lightInactiveLabel)
+                        
+                        Spacer()
+                        
+                        Text("\(viewModel.payment.satAmount) sat")
+                            .font(.mainFont(size: 14))
+                            .foregroundColor(Color.lightActiveLabel)
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Text("Description:")
+                            .font(.mainFont(size: 14))
+                            .foregroundColor(Color.lightInactiveLabel)
+                        
+                        Spacer()
+                        
+                        Text("\(viewModel.payment.description)")
+                            .font(.mainFont(size: 14))
+                            .foregroundColor(Color.lightActiveLabel)
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Text("Created:")
+                            .font(.mainFont(size: 14))
+                            .foregroundColor(Color.lightInactiveLabel)
+                        
+                        Spacer()
+                        
+                        Text("\(viewModel.payment.created)")
+                            .lineLimit(1)
+                            .font(.mainFont(size: 14))
+                            .foregroundColor(Color.lightActiveLabel)
+                    }
+                    .padding(.horizontal)
+                    
+                    if !viewModel.payment.isExpired {
+                        HStack {
+                            Text("Expires:")
+                                .font(.mainFont(size: 14))
+                                .foregroundColor(Color.lightInactiveLabel)
+                            
+                            Spacer()
+                            
+                            if let expireDate = viewModel.payment.expires {
+                                Text("\(expireDate)")
+                                    .lineLimit(1)
+                                    .font(.mainFont(size: 14))
+                                    .foregroundColor(Color.lightActiveLabel)
+                            } else {
+                                Text("-")
+                                    .font(.mainFont(size: 14))
+                                    .foregroundColor(Color.lightActiveLabel)
+                            }
+                        }
+                        .padding(.horizontal)
+                    } else {
+                        HStack {
+                            Text("Status:")
+                                .font(.mainFont(size: 14))
+                                .foregroundColor(Color.lightInactiveLabel)
+                            
+                            Spacer()
+                            
+                            Text("Expired")
+                                .font(.mainFont(size: 14))
+                                .foregroundColor(Color.lightActiveLabel)
+                        }
+                        .padding(.horizontal)
+                    }
+                    
+                    Text("Invoice:")
+                        .font(.mainFont(size: 14))
+                        .foregroundColor(Color.lightInactiveLabel)
+                        .padding(.vertical)
+                    
+                    if let invoice = viewModel.payment.invoice {
+                        Text("\(invoice)")
+                            .font(.mainFont(size: 12))
+                            .foregroundColor(Color.white)
+                            .padding(.horizontal)
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Share") {
+                        //                    showShareSheet.toggle()
+                    }
+                    .modifier(PButtonEnabledStyle(enabled: .constant(true)))
+                    .padding()
+                    //                .sheet(isPresented: $showShareSheet) {
+                    //                    ShareSheet(activityItems: [payment.invoice!])
+                    //                }
+                }
+                .padding()
+            }
+        }
     }
 }
 
