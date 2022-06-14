ea6b46ce81266845258b76a3fe92080c1fda0f22
cuhte3 <farid@getportal.co>
2022-06-14 22:34:38 +0700

    Update ChannelItemView.swift

--- Shared/Views/Lightning/ChannelItemView.swift
+++ Shared/Views/Lightning/ChannelItemView.swift
@@ -7,8 +7,77 @@
 
 import SwiftUI
 
-struct ChannelItemView: View {
+import Combine
+import LDKFramework_Mac
+
+class ChannelItemViewModel: ObservableObject {
     let channel: LightningChannel
+    let manager: ChannelManager
+    let service: ILightningDataService
+    
+    var channelID: [UInt8]? {
+        manager.list_channels().first(where: { $0.get_user_channel_id() == channel.id })?.get_channel_id()
+    }
+ 
+    init(channel: LightningChannel, channelManager: ChannelManager, dataService: ILightningDataService) {
+        self.channel = channel
+        self.manager = channelManager
+        self.service = dataService
+    }
+    
+    func closeChannel() {
+        if let channelID = channelID {
+            let result = manager.close_channel(channel_id: channelID)
+            if result.isOk() {
+                print("Channel cosed")
+                channel.state = .closed
+                service.update(channel: channel)
+            } else {
+                print("Channel closing error:")
+                let error = result.getError()
+                switch error?.getValueType() {
+                case .some(.ChannelUnavailable):
+                    print("Channel unavaliable")
+                case .some(.APIMisuseError):
+                    if let e = error?.getValueAsAPIMisuseError() {
+                        print("Api misuse error \(e.getErr())")
+                    }
+                case .some(.FeeRateTooHigh):
+                    if let e = error?.getValueAsFeeRateTooHigh() {
+                        print("Fee rate too high error \(e.getErr())")
+                    }
+                case .some(.RouteError):
+                    if let e = error?.getValueAsRouteError() {
+                        print("Router error \(e.getErr())")
+                    }
+                case .some(.IncompatibleShutdownScript):
+                    print("IncompatibleShutdownScript")
+                case .none:
+                    print("Unknown")
+                }
+            }
+        }
+    }
+}
+
+extension ChannelItemViewModel {
+    static func config(channel: LightningChannel) -> ChannelItemViewModel {
+        guard let manager = Portal.shared.lightningService?.manager.channelManager else {
+            fatalError("lightning service :/")
+        }
+        guard let dataService = Portal.shared.lightningService?.dataService else {
+            fatalError("lightning data service :/")
+        }
+        return ChannelItemViewModel(channel: channel, channelManager: manager, dataService: dataService)
+    }
+}
+
+struct ChannelItemView: View {
+    @StateObject private var viewModel: ChannelItemViewModel
+    
+    init(channel: LightningChannel) {
+        _viewModel = StateObject(wrappedValue: ChannelItemViewModel.config(channel: channel))
+    }
     
     var body: some View {
         ZStack {
@@ -21,7 +90,7 @@ struct ChannelItemView: View {
                         .foregroundColor(Color.lightInactiveLabel)
                     
                     Spacer()
-                    Text(channel.nodeAlias)
+                    Text(viewModel.channel.nodeAlias)
                         .foregroundColor(Color.white)
                     
                 }
@@ -33,7 +102,7 @@ struct ChannelItemView: View {
                     Spacer()
                     
                     HStack(spacing: 8) {
-                        Text(channel.state.description)
+                        Text(viewModel.channel.state.description)
                             .foregroundColor(Color.lightActiveLabel)
                         
 //                        if channel.state == .waitingFunds {
@@ -49,45 +118,14 @@ struct ChannelItemView: View {
                         .foregroundColor(Color.lightInactiveLabel)
                     
                     Spacer()
-                    Text("\(channel.satValue) sat")
+                    Text("\(viewModel.channel.satValue) sat")
                         .foregroundColor(Color.lightActiveLabel)
                     
                 }
                 
-                if channel.state == .open {
+                if viewModel.channel.state == .open {
                     Button {
-//                        guard let cm = PolarConnectionExperiment.shared.service?.manager.channelManager else { return }
-//                        if let channelID = cm.list_channels().first(where: { $0.get_user_channel_id() == channel.id })?.get_channel_id() {
-//                            let result = cm.close_channel(channel_id: channelID)
-//                            if result.isOk() {
-//                                print("Channel cosed")
-//                                channel.state = .closed
-//                                PolarConnectionExperiment.shared.service?.dataService.update(channel: channel)
-//                            } else {
-//                                print("Channel closing error:")
-//                                let error = result.getError()
-//                                switch error?.getValueType() {
-//                                case .some(.ChannelUnavailable):
-//                                    print("Channel unavaliable")
-//                                case .some(.APIMisuseError):
-//                                    if let e = error?.getValueAsAPIMisuseError() {
-//                                        print("Api misuse error \(e.getErr())")
-//                                    }
-//                                case .some(.FeeRateTooHigh):
-//                                    if let e = error?.getValueAsFeeRateTooHigh() {
-//                                        print("Fee rate too high error \(e.getErr())")
-//                                    }
-//                                case .some(.RouteError):
-//                                    if let e = error?.getValueAsRouteError() {
-//                                        print("Router error \(e.getErr())")
-//                                    }
-//                                case .some(.IncompatibleShutdownScript):
-//                                    print("IncompatibleShutdownScript")
-//                                case .none:
-//                                    print("Unknown")
-//                                }
-//                            }
-//                        }
+                        viewModel.closeChannel()
                     } label: {
                         Text("Close")
                             .font(.mainFont(size: 14))
@@ -110,7 +148,7 @@ struct ChannelItemView_Previews: PreviewProvider {
         ChannelItemView(
             channel: LightningChannel(id: Int16.random(in: 1...100), satValue: 2000, state: .open, nodeAlias: "Test node")
         )
-            .padding()
+        .padding()
     }
 }
 
