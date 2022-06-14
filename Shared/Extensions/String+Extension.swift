5e9361b289570fbcb4473b89766abdfc0b6984d0
cuhte3 <farid@getportal.co>
2022-06-14 22:32:49 +0700

    Update String+Extension.swift
    reversed

--- Shared/Extensions/String+Extension.swift
+++ Shared/Extensions/String+Extension.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 extension String {
+    var reversed: String {
+        let inputStringReversed = String(self.reversed())
+        let characters = Array(inputStringReversed)
+        var output = String()
+        
+        for i in 0...characters.count - 1 {
+            if i % 2 == 0 {
+                if (i+1) < characters.count {
+                    output.append(characters[i + 1])
+                }
+                output.append(characters[i])
+            }
+        }
+        
+        return output
+    }
+    
     func timestamp() -> TimeInterval {
         TimeInterval(exactly: self.doubleValue) ?? 0
     }
