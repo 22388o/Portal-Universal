30f3f3727b3e6742783f5ed1c040a5d16a07bddc
cuhte3 <farid@getportal.co>
2022-07-01 16:00:07 +0700

    Update ReceiveAssetsView.swift

--- Shared/Views/Wallet/ModalViews/ReceiveAssetsView.swift
+++ Shared/Views/Wallet/ModalViews/ReceiveAssetsView.swift
@@ -18,6 +18,7 @@ struct ReceiveAssetsView: View {
     }
     
     var body: some View {
+        #if os(macOS)
         ModalViewContainer(imageUrl: coin.icon, size: CGSize(width: 630, height: 310)) {
             VStack(spacing: 0) {
                 VStack(spacing: 16) {
@@ -80,6 +81,64 @@ struct ReceiveAssetsView: View {
         .onAppear {
             viewModel.update()
         }
+        #else
+        VStack(spacing: 0) {
+            VStack(spacing: 16) {
+                Text("Receive \(coin.name)")
+                    .font(.mainFont(size: 23))
+                    .foregroundColor(Color.coinViewRouteButtonActive)
+            }
+            .padding(.top, 57)
+            .padding(.bottom, 16)
+            
+            Divider()
+                .padding(.vertical)
+            
+            VStack {
+                if let image = viewModel.qrCode {
+                    image
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                }
+                VStack(alignment: .leading) {
+                    Text("Address")
+                        .font(.mainFont(size: 12))
+                        .foregroundColor(Color.coinViewRouteButtonActive)
+                    Text(viewModel.receiveAddress)
+                        .font(.mainFont(size: 14))
+                        .foregroundColor(Color.coinViewRouteButtonInactive)
+                    
+                    Spacer().frame(height: 25)
+                    
+                    HStack(spacing: 12) {
+                        PButton(label: "Copy to clipboard", width: 140, height: 32, fontSize: 12, enabled: true) {
+                            viewModel.copyToClipboard()
+                            
+                            if viewModel.isCopied != true {
+                                withAnimation {
+                                    viewModel.isCopied = true
+                                }
+                            }
+                        }
+                        .shadow(color: Color.pButtonShadowColor.opacity(0.1), radius: 6, x: 0, y: 4)
+                        
+                        if viewModel.isCopied {
+                            Text("Copied to clipboard!")
+                                .font(.mainFont(size: 12))
+                                .foregroundColor(.orange)
+                        }
+                    }
+                }
+                .padding(.leading)
+                
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 40)
+        .onAppear {
+            viewModel.update()
+        }
+        #endif
     }
 }
 
